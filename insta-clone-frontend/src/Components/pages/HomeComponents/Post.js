a0d8723d31008b726c701938eb3af5e272c618e7
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-06-21 21:18:18 +0200

    add loading for like/unlike and comment

--- insta-clone-frontend/src/Components/pages/HomeComponents/Post.js
+++ insta-clone-frontend/src/Components/pages/HomeComponents/Post.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useState, useRef } from "react";
+import Loading from "../../shared/Loading";
 
 export default function Post({
   title,
@@ -12,10 +13,12 @@ export default function Post({
 }) {
   const [postIsLiked, setIsLiked] = useState(isLiked);
   const [postComments, setComments] = useState(comments);
-  const commentInput = useRef(null);
+  const [commentInput, setCommentInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const onHeartClicked = async (e) => {
     console.log("heart clicked");
+    setIsLoading(true);
 
     const response = await fetch(`/posts/like`, {
       method: "PUT",
@@ -33,11 +36,13 @@ export default function Post({
     console.log(bodyOfResponse);
 
     setIsLiked(bodyOfResponse.isLiked);
+    setIsLoading(false);
   };
 
   const onSendClicked = async (e) => {
+    setIsLoading(true);
     console.log("send button clicked");
-    console.log(commentInput.current.value);
+    console.log(commentInput);
 
     const response = await fetch(`/posts/comment`, {
       method: "PUT",
@@ -48,54 +53,66 @@ export default function Post({
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
 
-      body: JSON.stringify({ post: _id, content: commentInput.current.value }),
+      body: JSON.stringify({ post: _id, content: commentInput }),
     });
 
     const bodyOfResponse = await response.json();
     console.log(bodyOfResponse);
     setComments(bodyOfResponse.comments);
-    commentInput.current.value = "";
+    setIsLoading(false);
+    setCommentInput("");
   };
 
   return (
     <div className="card home-card">
+      <></>
       <div className="postedBy">{postedBy}</div>
       <div className="card-image">
         <img src={imgUrl} alt="" />
       </div>
-      <div className="card-content">
-        <i
-          onClick={onHeartClicked}
-          style={postIsLiked ? { color: "red" } : { color: "black" }}
-          className="material-icons"
-        >
-          favorite
-        </i>
-        <h4>{title}</h4>
-        <p>{body}</p>
-        <div className="comment_and_send">
-          <input ref={commentInput} type="text" placeholder="add a comment" />
-          <i onClick={onSendClicked} className="material-icons">
-            send
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="card-content">
+          <i
+            onClick={onHeartClicked}
+            style={postIsLiked ? { color: "red" } : { color: "black" }}
+            className="material-icons"
+          >
+            favorite
           </i>
-        </div>
-        <div className="comments">
-          {postComments
-            .slice(0)
-            .reverse()
-            .map((comment) => (
-             
+          <h4>{title}</h4>
+          <p>{body}</p>
+          <div className="comment_and_send">
+            <input
+              value={commentInput}
+              onChange={(e) => setCommentInput(e.target.value)}
+              type="text"
+              placeholder="add a comment"
+            />
+            <i onClick={onSendClicked} className="material-icons">
+              send
+            </i>
+          </div>
+          <div className="comments">
+            {postComments
+              .slice(0)
+              .reverse()
+              .map((comment) => (
                 <div key={comment._id}>
-                  <div className="comment" >
-                    <div className="comment_author">{comment.writtenBy.name}</div>
+                  <div className="comment">
+                    <div className="comment_author">
+                      {comment.writtenBy.name}
+                    </div>
                     <div className="comment_content">{comment.content}</div>
                   </div>
                   <div className="divider"></div>
                 </div>
-              
-            ))}
+              ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
