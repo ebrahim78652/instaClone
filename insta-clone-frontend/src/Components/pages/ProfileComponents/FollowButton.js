f840eab23dd3d15d81ad6bf16f72492ff422c83b
ebrahim78652 <ebrahimkarjatwala@gmail.com>
2022-06-21 20:26:09 +0200

    add loading to follow and unfollow

--- insta-clone-frontend/src/Components/pages/ProfileComponents/FollowButton.js
+++ insta-clone-frontend/src/Components/pages/ProfileComponents/FollowButton.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import Loading from "../../shared/Loading";
 
 export default function FollowButton({ setNumFollowers, setIsFollowing }) {
   const user = useSelector(
     (state) => state.suggestedUserReducers.suggestedUser
   );
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const onClick = async () => {
     console.log("follow button clicked");
+    setIsLoading(true);
 
     const response = await fetch(`/user/follow/${user.name}`, {
       method: "PUT",
@@ -23,11 +28,18 @@ export default function FollowButton({ setNumFollowers, setIsFollowing }) {
     console.log(response);
     setNumFollowers(response.numFollowersUpdated);
     setIsFollowing((prev) => !prev);
+    setIsLoading(false);
   };
 
   return (
-    <button onClick={onClick} className="follow">
-      Follow
-    </button>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <button onClick={onClick} className="follow">
+          Follow
+        </button>
+      )}
+    </>
   );
 }
--- insta-clone-frontend/src/Components/pages/ProfileComponents/UnfollowButton.js
+++ insta-clone-frontend/src/Components/pages/ProfileComponents/UnfollowButton.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import Loading from "../../shared/Loading";
+import { useEffect, useState } from "react";
 
 export default function UnfollowButton({ setNumFollowers, setIsFollowing }) {
   const user = useSelector(
     (state) => state.suggestedUserReducers.suggestedUser
   );
+  const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
     console.log("Unfollow button clicked");
+    setIsLoading(true);
 
     const response = await fetch(`/user/unfollow/${user.name}`, {
       method: "PUT",
@@ -23,11 +27,18 @@ export default function UnfollowButton({ setNumFollowers, setIsFollowing }) {
     console.log(response);
     setNumFollowers(response.numFollowersUpdated);
     setIsFollowing((prev) => !prev);
+    setIsLoading(false);
   };
 
   return (
-    <button onClick={onClick} className="follow">
-      Unfollow
-    </button>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <button onClick={onClick} className="follow">
+          Unfollow
+        </button>
+      )}
+    </>
   );
 }
